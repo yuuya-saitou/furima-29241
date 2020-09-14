187707db7463e16121e98a5a1468ed90cd3b9b61
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-14 12:02:08 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+　before_action :basic_auth
+
+  def index
+    
+  end
+
+  private
+
+  def basic_auth
+    autenticate_or_request_wirh_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
