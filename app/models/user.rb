6452c9e63b2fad07d5348de0a582a6b4841a1078
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-17 16:57:09 +0900

    ユーザー登録情報のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, format: { with: /@.+/, message: '正しいメールアドレスを使用してください'}
+  validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: '6文字以上の半角英数字混合で入力してください'}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates :family_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
+  validates :family_name_kana
+  validates :first_name_kana
+  end
+
+  validates :birthday, presence: true
 end
