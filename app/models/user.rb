b6bfb973b6bb8a61cb2cb6a89367404c6077030e
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-18 15:12:50 +0900

    ユーザー新規登録、ログイン機能等の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,13 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: '6文字以上の半角英数字混合で入力してください'}
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-  validates :family_name
-  validates :first_name
+    validates :family_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
-  validates :family_name_kana
-  validates :first_name_kana
+    validates :family_name_kana
+    validates :first_name_kana
   end
 
   validates :birthday, presence: true
