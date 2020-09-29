ca343e734da6273be2214a56b2b7060c6ef2dfea
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-29 11:43:10 +0900

    Active Hash用各モデルの生成と記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_days
 
-  # validates :name, presence: true
-  # validates :explanation, presence: true
-  # validates :category_id, presence: true
-  # validates :status_id 
-  # validates :delivery_fee_id
-  # validates :shipping_area_id
-  # validates :shipping_days_id
-  # validates :price
-  # validates :user, foreign_key: true
+  validates :name, :explanation, :category_id, :status_id,
+            :delivery_fee_id, :shipping_area_id, :shipping_days_id, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :shipping_area_id,
+            :shipping_days_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}, presence: true
+  validates :user_id, presence: true
 end
