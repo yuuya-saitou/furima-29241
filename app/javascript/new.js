5baeb6a1b240278d9726be62ea09378da850991f
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-01 22:17:38 +0900

    商品出品画面における販売手数料と販売利益の表示

---
+++ app/javascript/new.js
@@ -0,0 +1,16 @@
+function price(){
+
+  const priceInput = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  priceInput.addEventListener('input', function(){
+    value = priceInput.value
+    addTaxPrice.innerHTML = value * 0.1
+    profit.innerHTML = value - (value * 0.1)
+  })
+
+  //addTaxPrice.getAttribute
+}
+
+window.addEventListener('load', price);
