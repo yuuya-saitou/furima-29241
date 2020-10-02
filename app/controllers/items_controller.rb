ce53e5c99f95d4fdda0846987b52f2672c089c03
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-02 19:10:26 +0900

    indexアクションとindex.html.erbの記述を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def index
+    @items = Item.all
+  end
 
   def new
     @item = Item.new
