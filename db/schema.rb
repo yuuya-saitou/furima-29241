00c0f5fc776afdc50cd33d9636adea287de63ee7
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-05 18:30:10 +0900

    itemモデルとマイグレーションファイルの再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_065721) do
+ActiveRecord::Schema.define(version: 2020_10_05_062601) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -67,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_09_23_065721) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
