899191fedda4e49db82ece368f064f6e46306bbc
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-05 20:40:50 +0900

    updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,13 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
