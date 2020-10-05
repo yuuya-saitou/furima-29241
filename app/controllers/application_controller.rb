54dce25a13524b18e1976208fd0ec39aa614e285
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-05 22:47:45 +0900

    buyerモデルの生成とbuyersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!, only: [:show]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  #def index
-  #end
-
-  #def new
-  #end
-
-  #def create
-  #end
 
   private
 
