62690f33dd59a2cc6c45b0c3c94bccd0b0d994d4
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-05 21:42:39 +0900

    destroyアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at ASC")
@@ -21,6 +21,14 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
       redirect_to root_path
     else
       render :edit
