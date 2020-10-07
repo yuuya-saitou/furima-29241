0c44fd19f0e8f73bf5f6be0a4c65f400278f83fe
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-07 14:20:36 +0900

    各テーブル間のアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :buyers
 
   with_options presence: true do
     validates :nickname
