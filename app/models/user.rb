a9f41b7b7684d13e1dfccadf38737185310e896d
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-07 14:17:27 +0900

    Userモデルの記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-
-  validates :nickname, presence: true
-  validates :email, presence: true, format: { with: /@.+/, message: '正しいメールアドレスを使用してください'}
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: '6文字以上の半角英数字混合で入力してください'}
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
-
-  validates :birthday, presence: true
   has_many :items
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: /@.+/, message: '正しいメールアドレスを使用してください'}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: '6文字以上の半角英数字混合で入力してください'}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' }
+    validates :birthday
+  end
 end
