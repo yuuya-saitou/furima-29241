5c1b7d3dcb84e549eed440b151dcbae65b5138dc
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-08 21:05:10 +0900

    orderコントローラー内の各アクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:id, :image, :name, :explanation, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,33 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
+
   def create
+    @order = BuyerOrder.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
