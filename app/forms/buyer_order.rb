6ce90baef37d8f095abcfd9871f3cf51ed349d88
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-10-08 21:09:10 +0900

    BuyerOrderクラスのテストコード記述

--- app/forms/buyer_order.rb
+++ app/forms/buyer_order.rb
@@ -7,7 +7,7 @@ class BuyerOrder
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'ハイフン（-）を入れて入力してください'}
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :municipality, format: { with: /\A[ぁ-んァ-ン一-龥]|[\d]\z/, message: 'ひらがな、カタカナ、漢字を使用してください'}
+    validates :municipality, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'ひらがな、カタカナ、漢字を使用してください'}
     validates :address, format: { with: /\A[ぁ-んァ-ン一-龥]|[\d]|[-]\z/}
     validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/, message: 'ハイフン（-）は入れずに11文字以内で入力してください'}
     validates :token
